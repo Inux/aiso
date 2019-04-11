0512d6c2329a1ec4fd90ab5b431f36fe8c9b527f
inux <inux.steve@gmail.com>
2019-04-11 20:38:34 +0200

    add part 5

--- src/HeuristicSearch/search.py
+++ src/HeuristicSearch/search.py
@@ -77,7 +77,6 @@ class GraphProblem():
             return is_in(state, self.goal)
         else:
             return state == self.goal
-
     def actions(self, A):
         """The actions at a graph node are just its neighbors."""
         return list(self.graph.get(A).keys())
@@ -90,6 +89,7 @@ class GraphProblem():
         return cost_so_far + (self.graph.get(A, B) or infinity)
 
 class Node:
+
     """A node in a search tree. Contains a pointer to the parent (the node
     that this is a successor of) and to the actual state for this node. Note
     that if a state is arrived at by two paths, then there are two nodes with
@@ -100,36 +100,22 @@ class Node:
     subclass this class."""
 
     def __init__(self, state, parent=None, action=None, path_cost=0):
-        """Create a search tree Node, derived from a parent by an action."""
-        self.state = state
-        self.parent = parent
-        self.action = action
-        self.path_cost = path_cost
-        self.depth = 0
-        if parent:
-            self.depth = parent.depth + 1
-
-
-    def expand(self, problem):
+        """ implement the attributes of the node class"""
+        raise NotImplementedError
+        
+    def exapand(self, problem):
         """List the nodes reachable in one step from this node."""
-        return [self.child_node(problem, action)
-                for action in problem.actions(self.state)]
-
+        raise NotImplementedError
+    
     def child_node(self, problem, action):
-        next_state = problem.result(self.state, action)
-        next_node = Node(next_state, self, action,
-                    problem.path_cost(self.path_cost, self.state,
-                                      action, next_state))
-        return next_node
-
+        """Return the child node reached by a given action"""
+        raise NotImplementedError
+        
     def solution(self):
         """Return the sequence of actions to go from the root to this node."""
-        return [node.action for node in self.path()[1:]]
-
+        raise NotImplementedError
+    
     def path(self):
         """Return a list of nodes forming the path from the root to this node."""
-        node, path_back = self, []
-        while node:
-            path_back.append(node)
-            node = node.parent
-        return list(reversed(path_back))
+        raise NotImplementedError
+
